5751265af8c8007799d28f42478989f32f74de9e
Tzu-Wei Huang <huang.dexter@gmail.com>
2018-11-10 18:05:08 +0800

    binary search 1

--- tests/test_test.py
+++ tests/test_test.py
@@ -3,6 +3,7 @@ def test_test():
     from examples import demo_graph
     from examples import demo_embedding
     from examples import demo_custom_scalars
+    exit()
     from examples import demo_multiple_embedding
     from examples import demo_purge
     from examples import demo_matplotlib
