5d5a2eb2482f466b9a6176f0c70d88495bc97009
Tzu-Wei Huang <huang.dexter@gmail.com>
2018-11-10 19:08:33 +0800

    binary search 2

--- tests/test_test.py
+++ tests/test_test.py
@@ -4,8 +4,8 @@ def test_test():
     from examples import demo_embedding
     from examples import demo_custom_scalars
     
-    #from examples import demo_multiple_embedding
-    #from examples import demo_purge
+    from examples import demo_multiple_embedding
+    from examples import demo_purge
     #from examples import demo_matplotlib
     #import subprocess
     #subprocess.check_output(['flake8', 'tensorboardX'])
