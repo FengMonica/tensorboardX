b346349ee81f0983176545d912fe4f9eda7e6666
Tzu-Wei Huang <huang.dexter@gmail.com>
2018-11-10 22:43:35 +0800

    remove sleep

--- examples/demo_purge.py
+++ examples/demo_purge.py
@@ -5,8 +5,8 @@ with SummaryWriter(log_dir='runs/purge') as w:
     for i in range(100):
         w.add_scalar('purgetest', i, i)
 
-sleep(1.0)
-print('sleep inside purge')
+#sleep(1.0)
+#print('sleep inside purge')
 with SummaryWriter(log_dir='runs/purge', purge_step=42) as w:
     # event 42~99 are removed (inclusively)
     for i in range(42, 100):
